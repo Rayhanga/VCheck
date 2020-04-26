659c4518a9943e0e2435cbd98107842cfd9ab738
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2020-04-26 21:50:06 +0700

    First initialization

---
+++ README.md
@@ -0,0 +1,66 @@
+# VCheck 
+
+VCheck is a python module to scrap data from Gunadarma's V-Class Website without hassle.
+
+## Installation
+
+Install `virtualenv`:
+
+```bash
+pip install virtualenv
+```
+
+Create new `virtualenv` and install depedencies:
+
+```bash
+virtualenv venv &&
+. venv/bin/activate && 
+pip install -r requirements.txt
+```
+
+Install `Selenium Webdriver` please refer to their documentation [Here](https://selenium-python.readthedocs.io/installation.html#drivers).
+
+## Usage
+
+Always use `virtualenv` when interacting with these modules
+
+```bash
+. venv/bin/activate
+```
+
+### Run on terminal
+
+```bash
+python vcheck.py
+```
+
+### Import as module
+
+```python
+from vcheck import VCheck
+
+driver = VCheck() # Create new instance of VCheck
+
+# Login as username with correct password
+driver.login(username, password)
+
+# Check login status
+driver.auth # returns True / False
+
+# Get a list of upcoming tasks
+upcomingTasks = driver.getUpcomingTask()
+
+# Get a list of current enroled course
+courseList = driver.getCourseList()
+
+# Terminate connection with VClass site
+driver.endConnection()
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[GNU GPLv3.0](https://github.com/Rayhanga/VCheck/blob/master/LICENSE)
---
+++ vcheck/scrapper.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+from bs4 import BeautifulSoup as bs
+
+def filterCourseName(course_name):
+    # Filter course name
+    ## Soft filter
+    softFilter = course_name.replace('Course name', '').replace('Course is starred', '').replace(' ','').replace('\n', '')
+    ## Hard filter
+    string = list(softFilter)
+    temp = []
+    c = 0
+    for i in range(len(string)):
+        if string[i] == '|':
+            c += 1
+        if c < 3 and c > 1:
+            temp.append(string[i])
+    ### Append some blank spaces
+    string = ''.join(temp).replace('|', '').replace('(*)','').replace('#', '').replace('/','')
+    temp = []
+    for i in range(len(string)):
+        if (string[i].isupper() or string[i].isdigit()) and i != 0:
+            temp.append(' ')
+        try:
+            if string[i]+string[i+1]+string[i+2] == 'dan':
+                temp.append(' ')
+        except:
+            pass
+        temp.append(string[i])
+    hardFilter = ''.join(temp)
+    tahun_ajar = softFilter[3:12]
+
+    if softFilter[:3] == 'ATA':
+        tahun = tahun_ajar[-4:]
+    else:
+        tahun = tahun_ajar[:4]
+        
+    return [hardFilter, tahun]
+
+def getCourseList(page_source):
+    # Get general info from enrolled course
+    sauce = bs(page_source, 'html.parser')
+    cList = sauce.findAll('li', {'class': 'course-listitem'})
+    data = []
+
+    for course in cList:
+        # Get anchor tag from html
+        cItem = course.find('a', {'class': 'coursename'})
+
+        # Course progress percentage
+        try:
+            progress = course.find('strong').text
+        except:
+            progress = None
+
+        # Course name
+        name = filterCourseName(cItem.text)[0]
+
+        # Course link
+        id = cItem.attrs.get('href')[-4:]
+
+        # Pack valuable info into a dictionary
+        data.append({
+            'courseId': id,
+            'courseName': name,
+            'courseProgress': progress
+        })
+
+    return(data)
+
+def getCourseData(page_source):
+    # Collect material, assignments (finished / unfinished) from specific course
+    sauce = bs(page_source, 'html.parser')
+    topicList = sauce.find('ul', {'class': 'topics'}).findChildren('li', {'class': 'section main clearfix'})
+
+    completed = 'https://v-class.gunadarma.ac.id/theme/image.php/boost/core/1585289926/i/completion-manual-y'
+
+    for topic in topicList:
+        topicName = topic.attrs.get('aria-label')
+        print(topic, '\n')
+          
+def getUpcomingTask(page_source):
+    # Get upcoming task
+    sauce = bs(page_source, 'html.parser')
+    taskList = sauce.find('div', {'class': 'eventlist my-1'}).findChildren('div', {'class': 'event m-t-1'})
+    data = []
+
+    for task in taskList:
+        now = datetime.now()
+        taskInfo = task.findAll('a')
+        today = now.strftime('%A, %d %B')
+        year = filterCourseName(taskInfo[-2].text)[1]
+
+        taskMatkul = filterCourseName(taskInfo[-2].text)[0]
+
+        taskDeadline = taskInfo[0].parent.text.replace('Today', today) + ', ' + year
+        taskDeadline = datetime.strptime(taskDeadline, '%A, %d %B, %I:%M %p, %Y')
+
+        taskUrl = taskInfo[-1].attrs.get('href')
+
+        if 'quiz' in taskUrl:
+            taskType = 'Quiz'
+        elif 'forum' in taskUrl:
+            taskType = 'Forum'
+        elif 'assign' in taskUrl:
+            taskType = 'Assignment'
+        else:
+            taskType = None
+
+        data.append({
+            'taskMatkul': taskMatkul,
+            'taskDeadline': taskDeadline.strftime('%d-%m-%Y %H:%M:%S'),
+            'taskUrl': taskUrl,
+            'taskType': taskType
+        })
+
+    return data
+
+def getLoginInfo(page_source):
+    sauce = bs(page_source, 'html.parser')
+    error = sauce.find('div', {'class': 'loginerrors'})
+
+    if error:
+        return False
+    else:
+        return True
